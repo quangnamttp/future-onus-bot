65b0ee792870a091ed2962ed3d56c64ca997e9eb
quangnamttp <quangnamttp@gmail.com>
2025-07-31 21:32:37 +0700

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("I'm alive!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Keep alive server running on port ${PORT}`);
+});
