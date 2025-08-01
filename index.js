534b65453c76a6c234fe379e4a90c5e1cf258be6
quangnamttp <quangnamttp@gmail.com>
2025-08-02 06:11:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ cron.schedule('0 6 * * *', async () => {
 });
 
 // Gửi lịch tin vĩ mô lúc 07:00 sáng mỗi ngày (theo giờ Việt Nam)
-cron.schedule('0 7 * * *', async () => {
+cron.schedule('* * * * *', async () => {
   if (botStatus !== "ON") return;
 
   console.log('[Cron] Gửi lịch tin vĩ mô 07:00');
